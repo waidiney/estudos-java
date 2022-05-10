a7b21b0c94cd5259ef989f61104836fa0829e267
Waidiney <dineyjr@hotmail.com>
2022-05-10 19:55:47 -0300

    adicionado dev-dojo

---
+++ dev-dojo/maratona-java/src/main/java/com/example/maratonajava/DevDojo.java
@@ -0,0 +1,8 @@
+package com.example.maratonajava;
+
+public class DevDojo {
+    public static void main(String[] args) {
+        String goy = "fEBEM";
+        System.out.println(goy);
+    }
+}
---
+++ dev-dojo/maratona-java/src/main/java/com/example/maratonajava/OlaDevDojo.java
@@ -0,0 +1,4 @@
+package com.example.maratonajava;
+
+public class OlaDevDojo {
+}
